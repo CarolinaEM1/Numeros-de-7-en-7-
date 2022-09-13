13592a70193448acd45b107f38f8c57de023525d
Carolina Martinez <carolinamtz091@gmail.com>
2022-09-13 14:52:16 -0500

    primera version

---
+++ src/ejercicio9/Ejercicio9.java
@@ -0,0 +1,22 @@
+/*
+ Escribir todos los números del 100 al 0 de 7 en 7.
+ */
+package ejercicio9;
+
+/**
+ *
+ * @author Carolina EM
+ */
+public class Ejercicio9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for (int i=100; i>=0; i-=7){
+            System.out.println(i);
+        }
+    }
+    
+}
